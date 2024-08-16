a0a76dd281f4baaefd5c6e1b5b34fcef64b6692e
devmnl <dev.mnlfranco@gmail.com>
2024-08-16 03:44:35 -0300

    upp

---
+++ frontend/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"1a4d3abaf3ca7fa9b40b17c6323b8a5d","url":"css/style.css"},{"revision":"bfc9724e12c979e469f44683c03644cd","url":"index.html"},{"revision":"f9b28677f5128fca8633e1d16b5ecf74","url":"js/script.js"},{"revision":"fb8d2bfc10a9d6ada535c8cfed6516b0","url":"service-worker.js"}]);
